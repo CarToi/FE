f4826ded3538690f9dde3943a4da359db47c5fa0
hyeonjiroh <sonamu6509@naver.com>
2025-07-08 10:21:07 +0900

    refactor: 장소 관련 상수 및 함수 분리

--- src/app/recommend/_components/SidePanel/SpaceCard.tsx
+++ src/app/recommend/_components/SidePanel/SpaceCard.tsx
@@ -1,4 +1,6 @@
 import { RecommendationResponse } from "@/lib/type";
+import { getCity } from "@/utils/getCity";
+import { CATEGORY } from "@/constants/spaceData";
 import Image from "next/image";
 import Temp from "@/assets/icons/temp.jpg";
 
@@ -26,7 +28,7 @@ export default function SpaceCard({
 
       <div className="flex gap-[8px] mt-[8px]">
         <div className="px-[12px] py-[4px] bg-[#F7F9FD] rounded-[6px] text-[#3560C0] text-[14px] font-[500] leading-[150%]">
-          {/* {category[spaceData.category]} */}
+          {CATEGORY[spaceData.category]}
         </div>
         {city && (
           <div className="px-[12px] py-[4px] bg-[#E9F5F7] rounded-[6px] text-[#3C98A4] text-[14px] font-[500] leading-[150%]">
@@ -37,22 +39,3 @@ export default function SpaceCard({
     </div>
   );
 }
-
-function getCity(position: string) {
-  if (position.indexOf("군산") > -1) {
-    return "군산";
-  }
-  if (position.indexOf("김제") > -1) {
-    return "김제";
-  }
-  if (position.indexOf("부안") > -1) {
-    return "부안";
-  }
-  return null;
-}
-
-// const category = {
-//   TOUR: "여행",
-//   CULTURE: "문화",
-//   FESTIVAL: "축제",
-// };
---
+++ src/utils/getCity.ts
@@ -0,0 +1,12 @@
+export function getCity(position: string) {
+  if (position.indexOf("군산") > -1) {
+    return "군산";
+  }
+  if (position.indexOf("김제") > -1) {
+    return "김제";
+  }
+  if (position.indexOf("부안") > -1) {
+    return "부안";
+  }
+  return null;
+}
