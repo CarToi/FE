d4406b454ebe84d02fdfedd327288980f68f90e0
hyeonjiroh <sonamu6509@naver.com>
2025-07-09 09:32:08 +0900

    feat: 지도에 마커 및 경로 표시 테스트

--- src/app/recommend/_components/MapView/index.tsx
+++ src/app/recommend/_components/MapView/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
+import { Map, MapMarker, Polyline, useKakaoLoader } from "react-kakao-maps-sdk";
 
 export default function MapView() {
   const [loading, error] = useKakaoLoader({
@@ -11,12 +11,31 @@ export default function MapView() {
   return (
     <Map
       level={3}
-      center={{ lat: 33.5563, lng: 126.79581 }}
+      center={{ lat: 35.9468421595771, lng: 126.5854974078118 }}
       style={{ width: "100%", height: "100%" }}
     >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Hello World!</div>
+      <MapMarker position={{ lat: 35.9468421595771, lng: 126.5854974078118 }}>
+        <div style={{ color: "#000" }}>새만금 국가산업단지</div>
       </MapMarker>
+      <MapMarker position={{ lat: 35.9468421595771, lng: 126.654457 }}>
+        <div style={{ color: "#000" }}>중간</div>
+      </MapMarker>
+      <MapMarker position={{ lat: 35.970944, lng: 126.5854974078118 }}>
+        <div style={{ color: "#000" }}>도착</div>
+      </MapMarker>
+      <Polyline
+        path={[
+          [
+            { lat: 35.9468421595771, lng: 126.5854974078118 },
+            { lat: 35.9468421595771, lng: 126.654457 },
+            { lat: 35.970944, lng: 126.5854974078118 },
+          ],
+        ]}
+        strokeWeight={5} // 선의 두께 입니다
+        strokeColor={"red"} // 선의 색깔입니다
+        strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle={"solid"} // 선의 스타일입니다
+      />
     </Map>
   );
 }
