221d4dccb3a1e29191fcf38b18e35d33761a1526
hyeonjiroh <sonamu6509@naver.com>
2025-07-09 12:27:12 +0900

    feat: 사용자가 선택한 거주 의향 지역을 도착 지점으로 지정 및 마커 표시 & 시작 지점과 중간 지점의 중간 지점을 초기 중심좌표로 지정

--- src/app/recommend/_components/MapView/index.tsx
+++ src/app/recommend/_components/MapView/index.tsx
@@ -1,41 +1,48 @@
 "use client";
 
-import { Map, MapMarker, Polyline, useKakaoLoader } from "react-kakao-maps-sdk";
+import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
+import { COORDINATE } from "@/constants/spaceData";
+import { getMidpoint } from "@/utils/getMidpoint";
+import { getDestination } from "@/utils/getDestination";
 
 export default function MapView() {
   const [loading, error] = useKakaoLoader({
     appkey: process.env.NEXT_PUBLIC_APPKEY!,
   });
+
   if (loading) return <div>Loading</div>;
   if (error) return <div>Error</div>;
+
+  const origin = COORDINATE.SAEMANGEUM;
+  const destination = getDestination();
+  const midpoint = getMidpoint(origin, destination);
+
   return (
-    <Map
-      level={3}
-      center={{ lat: 35.9468421595771, lng: 126.5854974078118 }}
-      style={{ width: "100%", height: "100%" }}
-    >
-      <MapMarker position={{ lat: 35.9468421595771, lng: 126.5854974078118 }}>
-        <div style={{ color: "#000" }}>새만금 국가산업단지</div>
-      </MapMarker>
-      <MapMarker position={{ lat: 35.9468421595771, lng: 126.654457 }}>
+    <Map level={7} center={midpoint} style={{ width: "100%", height: "100%" }}>
+      <MapMarker
+        position={origin}
+        image={{
+          src: "/icons/marker.svg",
+          size: {
+            width: 42,
+            height: 55,
+          },
+        }}
+      />
+      <MapMarker
+        position={destination}
+        image={{
+          src: "/icons/marker.svg",
+          size: {
+            width: 42,
+            height: 55,
+          },
+        }}
+      />
+
+      <MapMarker position={midpoint}>
         <div style={{ color: "#000" }}>중간</div>
       </MapMarker>
-      <MapMarker position={{ lat: 35.970944, lng: 126.5854974078118 }}>
-        <div style={{ color: "#000" }}>도착</div>
-      </MapMarker>
-      <Polyline
-        path={[
-          [
-            { lat: 35.9468421595771, lng: 126.5854974078118 },
-            { lat: 35.9468421595771, lng: 126.654457 },
-            { lat: 35.970944, lng: 126.5854974078118 },
-          ],
-        ]}
-        strokeWeight={5} // 선의 두께 입니다
-        strokeColor={"red"} // 선의 색깔입니다
-        strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        strokeStyle={"solid"} // 선의 스타일입니다
-      />
     </Map>
   );
 }
